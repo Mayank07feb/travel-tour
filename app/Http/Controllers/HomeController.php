8a9517a1355ea5f314dbb2b7575337e35aa22728
Mayank Sharma <makedy07feb@gmail.com>
2024-06-27 15:43:55 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('front_pages.index');
+    }
+
+    public function about()
+    {
+        return view('front_pages.about');
+    }
+
+    public function services()
+    {
+        return view('front_pages.services');
+    }
+
+    public function packages()
+    {
+        return view('front_pages.packages');
+    }
+
+    public function blog()
+    {
+        return view('front_pages.blog');
+    }
+
+    public function contact()
+    {
+        return view('front_pages.contact');
+    }
+
+    public function destination()
+    {
+        return view('front_pages.destination');
+    }
+
+    public function tour()
+    {
+        return view('front_pages.tour');
+    }
+
+    public function booking()
+    {
+        return view('front_pages.booking');
+    }
+
+    public function gallery()
+    {
+        return view('front_pages.gallery');
+    }
+
+    public function guides()
+    {
+        return view('front_pages.guides');
+    }
+
+    public function testimonial()
+    {
+        return view('front_pages.testimonial');
+    }
+
+    public function page404()
+    {
+        return view('front_pages.404');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/services', [HomeController::class, 'services'])->name('services');
+Route::get('/packages', [HomeController::class, 'packages'])->name('packages');
+Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/destination', [HomeController::class, 'destination'])->name('destination');
+Route::get('/tour', [HomeController::class, 'tour'])->name('tour');
+Route::get('/booking', [HomeController::class, 'booking'])->name('booking');
+Route::get('/gallery', [HomeController::class, 'gallery'])->name('gallery');
+Route::get('/guides', [HomeController::class, 'guides'])->name('guides');
+Route::get('/testimonial', [HomeController::class, 'testimonial'])->name('testimonial');
+Route::get('/page404', [HomeController::class, 'page404'])->name('page404');
